60854a87618ba5ea0ebe3ab5b6c78e027022d129
jack-cummings <jmcummi2@ncsu.edu>
2023-07-26 16:54:43 -0400

    Almost complete, submit does not work

--- main.py
+++ main.py
@@ -5,30 +5,55 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 import uvicorn
 import os
-
+import openai
 
 
 
 # Launch app and mount assets
 app = FastAPI()
-app.mount("macros/assets", StaticFiles(directory="macros/assets"), name="assets")
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 templates = Jinja2Templates(directory="templates/webapp")
+openai.api_key = os.environ['oaik']
 
 
 
 @app.get("/")
 async def home(request: Request):
     try:
-        # rest card template
-        out_html = ''
-
-        return templates.TemplateResponse('index_inset.html', {"request": request, 'card_inserts':out_html})
+        answer = ''
+        return templates.TemplateResponse('index.html', {"request": request, 'answer':answer})
 
     except Exception as e:
         print(e)
         return templates.TemplateResponse('error.html', {"request": request})
 
+@app.post('/query')
+async def query(request: Request):
+    try:
+        body = await request.body()
+        base = body.decode('UTF-8').strip()
+        print(base)
+        prompt = f"""today I've eaten {base}.
+         How many grams of protein and calories have I eaten? Show your work"""
 
+
+        # completion = openai.ChatCompletion.create(
+        #     model="gpt-3.5-turbo",
+        #     messages=[
+        #         {"role": "system", "content": "You are a helpful assistant."},
+        #         {"role": "user", "content": prompt}
+        #     ]
+        # )
+        #
+        # answer = completion.choices[0].message
+
+        answer=prompt
+
+        return templates.TemplateResponse('index.html', {"request": request, 'answer':answer})
+
+    except Exception as e:
+        print(e)
+        return templates.TemplateResponse('error.html', {"request": request})
 
 if __name__ == '__main__':
     if os.environ['MODE'] == 'dev':
